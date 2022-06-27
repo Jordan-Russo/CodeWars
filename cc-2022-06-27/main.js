d5c78bc1c35004493e67e65a6c09eb88b458a904
Jordan <jresiduum@gmail.com>
2022-06-27 11:00:16 -0400

    Completed code challenge for 2022-06-27

---
+++ cc-2022-06-27/main.js
@@ -0,0 +1,50 @@
+// url: https://www.codewars.com/kata/53d40c1e2f13e331fc000c26
+// The year is 1214. One night, Pope Innocent III awakens to find the the archangel Gabriel floating before him. Gabriel thunders to the pope:
+
+// Gather all of the learned men in Pisa, especially Leonardo Fibonacci. In order for the crusades in the holy lands to be successful, these men must calculate the millionth number in Fibonacci's recurrence. Fail to do this, and your armies will never reclaim the holy land. It is His will.
+
+// The angel then vanishes in an explosion of white light.
+
+// Pope Innocent III sits in his bed in awe. How much is a million? he thinks to himself. He never was very good at math.
+
+// He tries writing the number down, but because everyone in Europe is still using Roman numerals at this moment in history, he cannot represent this number. If he only knew about the invention of zero, it might make this sort of thing easier.
+
+// He decides to go back to bed. He consoles himself, The Lord would never challenge me thus; this must have been some deceit by the devil. A pretty horrendous nightmare, to be sure.
+
+// Pope Innocent III's armies would go on to conquer Constantinople (now Istanbul), but they would never reclaim the holy land as he desired.
+
+// In this kata you will have to calculate fib(n) where:
+
+// fib(0) := 0
+// fib(1) := 1
+// fin(n + 2) := fib(n + 1) + fib(n)
+// Write an algorithm that can handle n up to 2000000.
+
+// Your algorithm must output the exact integer answer, to full precision. Also, it must correctly handle negative numbers as input.
+function fib(n) {
+  if (n == 0||n == 1) {
+    return BigInt(n);
+  } else if (n >= 2 && n % 2 == 0) {
+    let k = n / 2;
+    let fk = fib(k);
+    return (2n * fib(k-1) + fk) * fk;
+  } else if (n >= 2) {
+    let k = (n + 1) / 2;
+    let fk1 = fib(k - 1);
+    let fk2 = fib(k);
+    return fk2*fk2 + fk1*fk1;
+  } else {
+    a = 0n;
+    b = 1n;
+    for (let i = 0; i > n; i--) {
+      [a, b] = [b - a, a];
+    }
+    return a;
+  }
+}
+
+let start = 1000000;
+let how_many = 1;
+for (let i = start; i < start + how_many ; i++) {
+  console.log(fib(i));
+}
