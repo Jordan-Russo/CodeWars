8099a3c512adf94b5f9a6b400931c5d9633931f7
Jordan <jresiduum@gmail.com>
2022-07-12 10:48:05 -0400

    Completed code challenge for 2022-07-12

---
+++ cc-2022-07-12/main.js
@@ -0,0 +1,27 @@
+// url: https://www.codewars.com/kata/576757b1df89ecf5bd00073b
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+function towerBuilder(nFloors) {
+  let tower = [];
+  for(let i = nFloors; i > 0; i--){
+    tower.unshift(" ".repeat(nFloors - i) + "*".repeat(1 + 2*(i-1)) + " ".repeat(nFloors - i))
+  }
+  return tower;
+}
