e7c2cfd3b74128f0d03a83deaa71faa6d6e26dae
Jordan <jresiduum@gmail.com>
2022-07-18 23:46:39 -0400

    Completed code challenge for 2022-07-18

---
+++ cc-2022-07-18/main.js
@@ -0,0 +1,3 @@
+// url: https://www.codewars.com/kata/523b623152af8a30c6000027
+// Now you have to write a function that takes an argument and returns the square of it.
+const square = (number) => number * number
