738f185a020973136e81284e2d46f7cb0bf6fdd3
Jordan <jresiduum@gmail.com>
2022-09-06 12:57:12 -0400

    Completed code challenge for 2022-09-06

---
+++ cc-2022-09-06/main.js
@@ -0,0 +1,11 @@
+// url: https://www.codewars.com/kata/564e7fc20f0b53eb02000106
+// Complete the function that takes a string of English-language text and returns the number of consonants in the string.
+
+// Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+function consonantCount(str) {
+  const consonantRegex = /[bcdfghjklmnpqrstvwxyz]/gi;
+  let consonateNum = 0;
+  let matches = str.match(consonantRegex);
+  if (matches) {consonateNum = matches.length}
+  return consonateNum;
+}
