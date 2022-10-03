1a8fac9c7f2f33066ee2253d7487f67c1b0bb039
Jordan <jresiduum@gmail.com>
2022-10-03 15:52:12 -0400

    Completed code challenge for 2022-10-03

---
+++ cc-2022-10-03/main.js
@@ -0,0 +1,5 @@
+// url: https://www.codewars.com/kata/583710ccaa6717322c000105
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+function simpleMultiplication(number) {
+  return number * (number % 2 ? 9 : 8);
+}
