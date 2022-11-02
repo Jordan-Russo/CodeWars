a7e4f62bfa26f3860055db66d0b27bb4eb247056
Jordan <jresiduum@gmail.com>
2022-11-02 13:36:35 -0400

    Completed code challenge for 2022-11-02

---
+++ cc-2022-11-02/main.js
@@ -0,0 +1,9 @@
+// url: https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+const replace = (string) => string.replace(/[aeiou]/gi, '!');
