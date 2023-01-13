98bc25b9dc1912f0a9ba6a516c9b068c7f21f529
Jordan <jresiduum@gmail.com>
2023-01-13 11:17:24 -0500

    Completed code challenge for 2023-01-13

---
+++ cc-2023-01-13/main.js
@@ -0,0 +1,6 @@
+// url: https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+function binToDec(string){
+  const number = parseInt(string, 2);
+  return number;
+}
