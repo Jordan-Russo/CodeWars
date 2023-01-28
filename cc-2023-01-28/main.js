9f302472bccb4476463f00fb47142bf66c81a8dd
Jordan <jresiduum@gmail.com>
2023-01-28 08:19:21 -0500

    Completed code challenge for 2023-01-28

---
+++ cc-2023-01-28/main.js
@@ -0,0 +1,35 @@
+// url: https://www.codewars.com/kata/56f3a1e899b386da78000732
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+// Examples of returns in different languages:
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+// or
+//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or 
+// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+
+function partlist(arr) {
+  let partArr = [];
+  for (let i = 0; i < arr.length - 1; i++){
+    const firstStr = arr.slice(0, i + 1).join(' ');
+    const lastStr = arr.slice(i + 1).join(' ');
+    partArr.push([firstStr, lastStr]);
+  }
+  return partArr;
+}
+// this function gives all the ways to divide a list of at least two elements into two non-empty parts
+// we do two slices of the array and join the slices together ( 2 arrays => 2 strings )
+// to do this there must always be at least one element in each slice
+// write a for loop that does that assuming is always arr.length >= 2
