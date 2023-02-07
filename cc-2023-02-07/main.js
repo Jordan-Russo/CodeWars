ba4e1ec9d76364758638258ccf97ad1d45210bec
Jordan <jresiduum@gmail.com>
2023-02-07 15:25:12 -0500

    Completed code challenge for 2023-02-07

---
+++ cc-2023-02-07/main.js
@@ -0,0 +1,37 @@
+// url: https://www.codewars.com/kata/53697be005f803751e0015aa
+
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+// For example, decode("h3 th2r2") would return "hi there".
+
+const vowelToNum = {
+  a: 1,
+  e: 2,
+  i: 3, 
+  o: 4,
+  u: 5,
+};
+
+function encode(string) {
+  return [...string].map(char => vowelToNum[char] || char).join('');
+}
+
+function decode(string) {
+  return [...string].map(char => {
+    for (let vowel in vowelToNum){
+      if(char == vowelToNum[vowel]){
+        return vowel;
+      }
+    }
+    return char;
+  }).join('');
+}
