2a0c768c7e69f9df0dbd57fd733b25a16201c657
Jordan <jresiduum@gmail.com>
2023-02-16 12:11:39 -0500

    Completed code challenge for 2023-02-16

---
+++ cc-2023-02-16/main.js
@@ -0,0 +1,30 @@
+// url: https://www.codewars.com/kata/5680781b6b7c2be860000036
+// We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
+// NOTES
+// Vowels in this context refers to: a e i o u y (including upper case)
+// This is indexed from [1..n] (not zero indexed!)
+
+function vowelIndices(word){
+  let arr = [];
+  for (let i = 0; i < word.length; i++){
+    if('aeiouy'.includes(word[i].toLowerCase())){
+      arr.push(i+1);
+    }
+  }
+  return arr;
+}
+// return [...word].reduce((arr, char, i) => {
+//     if ('aeiouy'.includes(char.toLowerCase())){
+//       arr.push(i + 1);
+//       return arr;
+//     }
+//     return arr;
+//   }, [])
