23f21fb4a9848e29fde9d734cb896df613e7d9d9
Jordan <jresiduum@gmail.com>
2023-03-20 20:29:13 -0400

    Completed code challenge for 2023-03-20

---
+++ cc-2023-03-20/main.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Variable assignment
+// DEBUGGING CHALLENGE
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
+// name is a keyword so this is a bad idea to do
