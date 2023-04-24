dc612760a4489b96df6e190f46854c6d07878063
Jordan <jresiduum@gmail.com>
2023-04-24 12:34:30 -0400

    Quickfix for 2023-04-24

--- cc-2023-04-24/main.js
+++ cc-2023-04-24/main.js
@@ -1,62 +1,80 @@
-url: https://www.codewars.com/kata/57b6f5aadb5b3d0ae3000611
+// url: https://www.codewars.com/kata/54b724efac3d5402db00065e
+// morsecode: https://en.wikipedia.org/wiki/Morse_code
 
-// Determine length of missing array
+// given a string of morse code
+// 1 space seperates characters
+// 3 spaces seperate words
+// all characters are capitalized
+// morse should be trimmed before read
 
-// Given an array of arrays, anything can be inside those arrays but it will only be an array within another array. 
-// Edge case: We can also be given null or an empty array
+// return the translated morse code into english.
 
-// Return the missing length of array
-// Will Always be inbetween the smallest and largest array length in the array
-// Edge Case: For null and empty array return 0
+// decodeMorse('···· · −·−−   ·−−− ··− −·· ·') 
+// HEY JUDE
+// decodeMorse('      ·−   −·−· ·− − ')
 
-// getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]) === 3
-// getLengthOfMissingArray([[1, 2], [4, 5, 1], [1], [5, 6, 7, 8, 9]]) === 4
-// getLengthOfMissingArray([[1,2], [2,4,3], [2,2,2,2], [1,2,3,4,5,6]]) === 5
-
-// Edge checks array is null or array length 0, return 0
-// Iterate through the array
-// Create 3 variables
-// minimum length
-// maximum length
-// sum of lengths
-// One helper function:
-// function to get cumulative sum between 1 and length, cumulativeSum(length)
-// expectedSum = cumulativeSum(maxLength) - cumulativeSum(minLength - 1)
-// let missingLength = expectedSum - sum
-// return missingLength
-
-
-
-function getLengthOfMissingArray(arrayOfArrays){
-  if(!arrayOfArrays || arrayOfArrays.length === 0){return 0}
-  let minLength = Infinity, maxLength = 0, sumLength = 0
-  function cumulativeSum(length){
-    return length * ((1 + length)/2)
+// Optimized O(n)
+// start with trim
+// have a stored msg, word, and char
+// loop through the morse
+  // if our morse is a space
+    // evaluate the morseChar, then add it to morse word, empty morseChar
+  // if the previous three characters in the morse were spaces 
+    // add (morseWord + ' ') to message and empty morseWord
+  // if our morse is not a space 
+    // add the char to the morseChar
+// evaluate the last morseChar, add it to morseWord, and add that to the message
+// trim the extra off the message
+// return the msg
+function decodeMorse(morse){
+  morse = morse.trim()
+  let message = ''
+  let morseWord = ''
+  let morseChar = ''
+  for(let char in morse){
+    if(morse[char] === ' '){
+      morseWord += MORSE_CODE[morseChar] || ''
+      morseChar = ''
+    }
+    if(morse.slice(char - 3, char) === '   '){
+      message += `${morseWord} `
+      morseWord = ''
+    }
+    if(morse[char] !== ' '){
+      morseChar += morse[char]
+    }
   }
-  for(let array of arrayOfArrays){
-    if(!array || array.length === 0){return 0}
-    const length = array.length
-    if(length < minLength){minLength = length}
-    if(length > maxLength){maxLength = length}
-    sumLength += length
-  }
-  const expectedSum = cumulativeSum(maxLength) - cumulativeSum(minLength - 1)
-  const missingLength = expectedSum - sumLength
-  return missingLength
+  // last character
+  morseWord += MORSE_CODE[morseChar]
+  message += morseWord
+
+  return message.trimStart()
 }
-// Better option: 
-// map the lengths with optional chain
-// put it into a set
-// set.has(0) or set.has(undefined) in O(1) to return 0
-// then determine min in array and make a loop asking whether the next ascending number exists until it's false and return that number
 
-function getLengthOfMissingArray(arrayOfArrays){
-  if(!arrayOfArrays || arrayOfArrays.length === 0){return 0}
-  const arrLengths = arrayOfArrays.map(arr => arr?.length)
-  const lengthSet = new Set(arrLengths)
-  const maxLength = Math.max(...lengthSet)
-  if(lengthSet.has(0) || lengthSet.has(undefined)){return 0}
-  for(let i = maxLength - 1; i > 0; i--){
-    if(!lengthSet.has(i)){return i}
-  }
+// O(n**2) unoptimized old code
+// trim the morse code
+// make an array from splitting the code by '   '
+// nest another array by splitting each character by ' '
+// convert each element in the array of arrays to letters
+// join chracters together with ''
+// join words together with ' '
+// return the resulting string
+
+
+// Given the object MORSE_CODE that has keys of all the morse code character strings
+
+function decodeMorse(morse){
+  morse = morse.trim()
+  // '···· · −·−−   ·−−− ··− −·· ·'
+  const words = morse.split('   ')
+  // ['···· · −·−−', '·−−− ··− −·· ·']
+  .map(word => word.split(' ')
+  // [['····', '·', '−·−−'], ['·−−−', '··−', '−··', '·']]
+  .map(char => MORSE_CODE[char])
+  // ['H', 'E', 'Y']
+  .join(''))
+  // ['HEY', 'JUDE']
+  .join(' ')
+  // 'HEY JUDE'
+  return words
 }
