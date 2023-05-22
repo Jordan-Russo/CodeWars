cdcd07fccd0f549251426c98a21ab653188342c0
Jordan <jresiduum@gmail.com>
2023-05-22 11:07:58 -0400

    Completed coding challenge for 2023-05-22

---
+++ cc-2023-05-22/main.js
@@ -0,0 +1,14 @@
+// url: https://www.codewars.com/kata/55acfc59c3c23d230f00006d
+// get ascii value of character
+
+// given a character as a string
+// return the ASCII value it has
+// Examples:
+  //  getASCII('!') => 33
+  //  getASCII('A') => 65
+  //  getASCII(' ') => 32
+
+// Get the ASCII value of the first charatcter in the string
+// Return the ASCII value number
+
+const getASCII = char => char.charCodeAt(0)
